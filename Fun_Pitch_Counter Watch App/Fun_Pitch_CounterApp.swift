4a49f8a00af12da16dc09acdde542bee475187b2
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2025-09-21 17:39:52 -0400

    First Commit

---
+++ Fun_Pitch_Counter Watch App/Fun_Pitch_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_Pitch_CounterApp.swift
+//  Fun_Pitch_Counter Watch App
+//
+//  Created by Kevin.McEwan.a1.2 on 2025-09-21.
+//
+
+import SwiftUI
+
+@main
+struct Fun_Pitch_Counter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
